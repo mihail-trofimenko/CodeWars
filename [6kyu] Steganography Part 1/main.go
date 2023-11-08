3d1d205688b959c21c18ffe12f8db1d8772b4daf
mihail-trofimenko <mihail.trofimenko@gmail.com>
2023-11-08 23:09:18 +0700

    1

--- [6kyu] Steganography Part 1/main.go
+++ [6kyu] Steganography Part 1/main.go
@@ -5,10 +5,13 @@ import (
 	"strconv"
 )
 
+// заполняем массив, содержащий маску соответственно старшинству разрядов в исходном байте:
+// номер элемента массива - это степень 2 (базы двоичной системы)
+
 func getBits(b byte) [8]byte {
 	var sb [8]byte
 	for i := 0; i < 8; i++ {
-		sb[i] = byte(int(b)>>i) & 1
+		sb[7-i] = byte(int(b)>>i) & 1
 	}
 	return [8]byte(sb)
 }
@@ -16,27 +19,55 @@ func getBits(b byte) [8]byte {
 func inject(orig [3][3]byte, mask [8]byte) [3][3]byte {
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
-			if i+j < 8 {
-				orig[i][j] = orig[i][j] | mask[i+j]
+			if i == 2 && j == 2 {
+				break
+			} else {
+				orig[i][j] = orig[i][j] | mask[i*3+j]
 			}
 		}
 	}
 	return orig
 }
 
-func Conceal(msg string, pixels [][]uint8) [][]uint8 {
+type pixel3 [3][3]byte
 
-	// your code here
-	return nil
+func PrintPixel(tp pixel3) {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if i == 2 && j == 2 {
+				break
+			} else {
+				fmt.Printf("[%d][%d]:%s\t", i, j, strconv.FormatInt(int64(tp[i][j]), 2))
+			}
+		}
+		fmt.Println()
+	}
 }
 
-type pixel3 [3][3]byte
+// пихаем по байту в каждые 3 пикселя
+func Conceal(msg string, pixels [][]uint8) [][]uint8 {
+
+	// из условия задачи:
+	// Your code should return nil/None if there
+	// aren't enough pixels to conceal the message.
+	if int((len(pixels))/3) < len(msg) {
+		return nil
+	}
+
+	var n byte = 204 // байт для инжекции
+	var sn [8]byte   // маска из 8 байтов, полученная из n
+	var tp pixel3    // входной массив, трёх пикселей RGB
+
+}
 
 func main() {
 
-	var n byte = 204
-	var sn [8]byte
+	var n byte = 204 // байт для инжекции
+	var sn [8]byte   // маска из 8 байтов, полученная из n
+
+	// входной массив, трёх пикселей RGB
 	var tp = pixel3{{254, 254, 254}, {254, 254, 254}, {254, 254, 254}}
+
 	fmt.Println(strconv.FormatInt(int64(n), 2), " - MASK")
 
 	sn = getBits(n)
@@ -46,12 +77,11 @@ func main() {
 
 	fmt.Println(" - MASK in array")
 
+	fmt.Println("Исходный массив:")
+	PrintPixel(tp)
+
 	tp = inject(tp, sn)
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			fmt.Println(strconv.FormatInt(int64(tp[i][j]), 2))
-		}
 
-	}
-
+	fmt.Println("Массив под маской:")
+	PrintPixel(tp)
 }
