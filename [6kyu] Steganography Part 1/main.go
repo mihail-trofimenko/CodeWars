905624e072ed6b0223995f96ad150644d6668957
mihail-trofimenko <mihail.trofimenko@gmail.com>
2023-11-09 21:39:35 +0700

    Первая рабочая версия

--- [6kyu] Steganography Part 1/main.go
+++ [6kyu] Steganography Part 1/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	//"strconv"
 )
-
-// заполняем массив, содержащий маску соответственно старшинству разрядов в исходном байте:
-// номер элемента массива - это степень 2 (базы двоичной системы)
 
 func getBits(b byte) [8]byte {
 	var sb [8]byte
@@ -16,73 +13,55 @@ func getBits(b byte) [8]byte {
 	return [8]byte(sb)
 }
 
-func inject(orig [3][3]byte, mask [8]byte) [3][3]byte {
+func inject(orig [][]uint8, mask [8]uint8) [][]uint8 {
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
 			if i == 2 && j == 2 {
 				break
 			} else {
-				orig[i][j] = orig[i][j] | mask[i*3+j]
+				switch mask[i*3+j] {
+				case 0:
+					orig[i][j] &= 254
+				case 1:
+					orig[i][j] |= 1
+				}
 			}
 		}
 	}
 	return orig
 }
 
-type pixel3 [3][3]byte
-
-func PrintPixel(tp pixel3) {
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			if i == 2 && j == 2 {
-				break
-			} else {
-				fmt.Printf("[%d][%d]:%s\t", i, j, strconv.FormatInt(int64(tp[i][j]), 2))
-			}
-		}
-		fmt.Println()
-	}
-}
-
 // пихаем по байту в каждые 3 пикселя
 func Conceal(msg string, pixels [][]uint8) [][]uint8 {
+	// массив из трёх пикселей RGB на корм inject()
+	var tp = [][]uint8{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
 
-	// из условия задачи:
-	// Your code should return nil/None if there
-	// aren't enough pixels to conceal the message.
 	if int((len(pixels))/3) < len(msg) {
 		return nil
 	}
-	/*
-		var n byte = 204 // байт для инжекции
-		var sn [8]byte   // маска из 8 байтов, полученная из n
-		var tp pixel3    // входной массив, трёх пикселей RGB
-	*/
 
+	for i := 0; i < len(msg); i++ {
+		for j := 0; j < 3; j++ {
+			tp[j] = pixels[i*3+j]
+		}
+		tp = inject(tp, getBits(msg[i]))
+		for j := 0; j < 3; j++ {
+			pixels[i*3+j] = tp[j]
+		}
+
+	}
+	return pixels
 }
 
 func main() {
 
-	var n byte = 204 // байт для инжекции
-	var sn [8]byte   // маска из 8 байтов, полученная из n
-
-	// входной массив, трёх пикселей RGB
-	var tp = pixel3{{254, 254, 254}, {254, 254, 254}, {254, 254, 254}}
-
-	fmt.Println(strconv.FormatInt(int64(n), 2), " - MASK")
-
-	sn = getBits(n)
-	for i := 0; i < 8; i++ {
-		fmt.Print(strconv.FormatInt(int64(sn[i]), 2))
+	var pix = [][]uint8{
+		{255, 0, 0}, {0, 255, 0}, {0, 0, 255},
+		{255, 0, 0}, {0, 255, 0}, {0, 0, 255},
+		{169, 105, 71}, {172, 211, 192}, {181, 140, 38},
+		{108, 58, 63}, {105, 235, 16}, {204, 69, 21},
+		{24, 40, 224}, {88, 84, 121}, {123, 41, 163},
 	}
+	fmt.Println(Conceal("Hello", pix))
 
-	fmt.Println(" - MASK in array")
-
-	fmt.Println("Исходный массив:")
-	PrintPixel(tp)
-
-	tp = inject(tp, sn)
-
-	fmt.Println("Массив под маской:")
-	PrintPixel(tp)
 }
