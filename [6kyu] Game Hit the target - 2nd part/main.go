75077974998c664ffc076327e61f8ae5aa44b9fd
mihail-trofimenko <mihail.trofimenko@gmail.com>
2023-11-12 12:38:01 +0700

    Проблема с рандом тестами на CodeWars

---
+++ [6kyu] Game Hit the target - 2nd part/main.go
@@ -0,0 +1,101 @@
+package main
+
+import "fmt"
+
+var arrow struct {
+	x         int
+	y         int
+	dir_x     int
+	direction string ""
+	aimed     bool
+}
+
+func Solution(mtrx [][]rune) bool {
+	arrow.direction = ""
+	arrow.aimed = false
+	fmt.Println()
+	for y := 0; y < len(mtrx); y++ {
+		for x := 0; x < len(mtrx[y]); x++ {
+			fmt.Printf("[%s]", string(mtrx[y][x]))
+		}
+		fmt.Println()
+	}
+
+	for y := 0; y < len(mtrx); y++ {
+		for x := 0; x < len(mtrx[y]); x++ {
+			if mtrx[y][x] == '>' || mtrx[y][x] == '<' || mtrx[y][x] == 'v' || mtrx[y][x] == '^' {
+				arrow.y = y
+				arrow.x = x
+				switch mtrx[y][x] {
+				case '>':
+					arrow.direction = "right"
+				case '<':
+					arrow.direction = "left"
+				case '^':
+					arrow.direction = "up"
+				case 'v':
+					arrow.direction = "down"
+				}
+				break
+			}
+		}
+	}
+
+	switch arrow.direction {
+	case "right":
+		{
+			for x := arrow.x; x < len(mtrx[arrow.y]); x++ {
+				if mtrx[arrow.y][x] == 'x' {
+					arrow.aimed = true
+				}
+			}
+		}
+
+	case "left":
+		{
+			for x := arrow.x; x >= 0; x-- {
+				if mtrx[arrow.y][x] == 'x' {
+					arrow.aimed = true
+				}
+			}
+		}
+
+	case "down":
+		{
+			for y := arrow.y; y < len(mtrx); y++ {
+				if mtrx[y][arrow.x] == 'x' {
+					arrow.aimed = true
+				}
+			}
+		}
+
+	case "up":
+		{
+			for y := 0; y < arrow.y; y++ {
+				if mtrx[y][arrow.x] == 'x' {
+					arrow.aimed = true
+				}
+			}
+		}
+	}
+	fmt.Println("Arrow is aimed:", arrow.aimed)
+	return arrow.aimed
+}
+
+func main() {
+
+	mtrx := [][]rune{
+		{'^', ' ', ' '},
+		{'x', ' ', ' '},
+		{' ', ' ', ' '}}
+	fmt.Println(Solution(mtrx))
+
+	mtrx = [][]rune{
+		{' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', 'v', 'x'}}
+	fmt.Println(Solution(mtrx))
+
+}
