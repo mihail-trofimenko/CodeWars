5666f518cdc20f77afadfc2668da5d3872f655ff
mihail-trofimenko <mihail.trofimenko@gmail.com>
2023-11-15 20:33:30 +0700

    Можно было красивее через map и счётчик повторений

---
+++ 5kyu_First_non-repeating_character/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func FirstNonRepeating(str string) string {
+
+	for j, r0 := range str {
+		repeating := false
+		for i, r1 := range str {
+			if ((r1 >= 'A') && (r1 <= 'z')) && ((r0 >= 'A') && (r0 <= 'z')) && ((r1 == r0+32) || (r0 == r1+32) || r0 == r1) && i != j {
+				repeating = true
+				break
+			}
+			if ((r0 < 'A') || (r0 > 'z')) && r0 == r1 && i != j {
+				repeating = true
+				break
+			}
+		}
+		if repeating == false {
+			return string(r0)
+		}
+	}
+
+	return ""
+}
+
+func main() {
+	fmt.Println(FirstNonRepeating("ZBgAOv:Al:c;eu4YcM:guJ7"))
+}
